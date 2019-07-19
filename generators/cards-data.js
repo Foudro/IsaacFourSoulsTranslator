3a62508a37538d20a6805d111b36051a6f2eb725
VANSON Corentin <corentin.vanson.externe@swisslife.fr>
2019-07-19 17:53:41 +0200

    update csv

--- generators/cards-data.js
+++ generators/cards-data.js
@@ -1,13 +1,31 @@
 const {promises: fs} = require('fs');
 
 (async () => {
-    const dirPath = 'public/cards';
+    const dirPath = 'generators';
     const dir = await fs.readdir(dirPath);
-    const cards = dir.map(card => {
-        return {
-            filename: 'cards/' + card,
-            name: card.substr(0, card.lastIndexOf("."))
-        };
-    });
+    const cards = await Promise.all(
+        dir.map(async card => {
+            const file = await fs.readFile(dirPath + '/' + card);
+            const famillyCard = await Promise.all(
+                file.toString().split('$').map(async line => {
+                    const elements = line.split(';');
+                    const dir = await fs.readdir('public/cards');
+                    dir.forEach(img => {
+                        
+                    });
+                    return {
+                        originalName: elements[0],
+                        translatedName: elements[1],
+                        translatedText: elements[2]
+                    };
+                })
+            );
+            return {
+                category: card.replace(/.csv/, ''),
+                cards: famillyCard
+            };
+        })
+    );
+    console.log(cards);
     await fs.writeFile('src/cards-data.json', JSON.stringify(cards));
 })()
